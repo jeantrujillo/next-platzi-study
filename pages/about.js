26caf5d4c0639c4977173c3439ddf86781698208
jeantrujillo <jean.trujillo@sofka.com.co>
2022-01-11 09:06:23 -0500

    Add navbar w/ SPA

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
 
 const About = () => {
     return (
         <div>
+            <Navbar/>
+            <p>
             Esta es la pagina de about
+            </p>
         </div>
     )
 }
